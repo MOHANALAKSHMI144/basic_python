04e067c73d0f1d5f0caa5b2460d66b10758ee2eb
MOHANALAKSHMI144 <mohanakarri75@gmail.com>
2025-03-12 15:44:32 +0530

    Update prime_range.py

--- prime_range.py
+++ prime_range.py
@@ -17,8 +17,15 @@ def print_primes(start, end):
     primes = [num for num in range(start, end + 1) if is_prime(num)]
     print("Prime numbers in the range:", ", ".join(map(str, primes)))
 
-# Accept user input for start and end range
+
 start = int(input("Enter the start of the range: "))
 end = int(input("Enter the end of the range: "))
 
 print_primes(start, end)
+
+otput:
+Enter the start of the range: 7
+Enter the end of the range: 200
+Prime numbers in the range: 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199
+
+
